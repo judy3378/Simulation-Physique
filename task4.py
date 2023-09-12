dc3b9ddb0970bf94acc9d420611beb9d6039a393
Juyeon <81371179+judy3378@users.noreply.github.com>
2023-09-12 12:03:09 +0200

    Update and rename task3.py to task4.py

---
+++ task4.py
@@ -0,0 +1,85 @@
+
+import pygame
+from vecteur3D import *
+from Particule import *
+from random import random
+from pylab import legend, show
+from math import pi
+import sys
+
+# Task 4: Simulate a pendulum
+# Initialisation Pygame
+def main():
+
+    # Simulation title
+    title = "Pendulum"
+
+    # initializing pygame
+    pygame.init()
+
+    # clock object to ensure 
+    clock = pygame.time.Clock()
+
+    # set up screen
+    # top left corner is (0,0) top right (1000,0) bottom left (0,700)
+    # bottom right (1000,700).
+    scene_width = 10
+    scene_height = 7
+    #screen = pygame.display.set_mode((1000, 700))
+    pygame.display.set_caption(title)
+
+    # Create the Universe
+    plage = Univers(step=0.01, dimensions=(scene_width*100,scene_height*100), scale=100)
+
+    # Parameters for the particles
+    num_particles = 10
+
+    # Creating particles
+    p1 = Particule(pos0=Vecteur3D(4,4,0), vit0 = Vecteur3D(0,0,0), mass = 1, name = 'ball 1', color = 'green', fix=True)
+    p2 =  Particule(pos0=Vecteur3D(6,3,0), vit0 = Vecteur3D(0,0,0), mass = 1, name = 'ball 2', color= 'blue')
+    plage.addAgent(p1)
+    plage.addAgent(p2)
+
+    l0 =( p1.getPosition() - p2.getPosition() ).mod()
+    #ressort = dampingSpring(p1,p2, k = 1, c = 0, l0 = l0)
+    #plage.addSource(ressort)
+
+    rod = Rod(p1,p2)
+    plage.addSource(rod)
+
+    # Adding gravity
+    plage.addSource(Gravity(Vecteur3D(0, 9.8,0)))
+
+    # Initialize Pygame for visualization (optional)
+    plage.gameInit()
+
+    print ('--------------------------------')
+    print ('Usage:')
+    print ('Press (q) to quit simulation. \nand see figure of trajectories.')
+    print ('--------------------------------')
+
+
+    while plage.run:
+        # 30 fps
+        clock.tick(30)
+
+        plage.gameUpdate()
+        
+
+        if plage.gameKeys[K_q]:
+            plage.run = False
+        if plage.gameKeys[K_ESCAPE]:
+            plage.run = False
+
+    pygame.quit()
+        
+    plage.plot()
+    legend()
+    show()
+    sys.exit(0)
+    
+    
+    
+
+if __name__ == '__main__':
+    main()
